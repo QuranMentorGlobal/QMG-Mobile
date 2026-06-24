163dbc11f08025e3056e84d2e21fea4f8e079f24
QuranMentorGlobal <quranmentorglobal@gmail.com>
2026-06-24 09:44:30 +0500

    Delete src directory

--- src/assets/README.txt
+++
@@ -1,4 +0,0 @@
-Replace these placeholders with brand assets before building a standalone binary:
-  - icon.png   (1024x1024)  app icon
-  - splash.png (1284x2778)  native splash, charcoal #0B0B0B background, gold mark
-Expo Go runs fine without them during development.
--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Screen } from './Screen';
-export { Button } from './Button';
-export { Input } from './Input';
-export { Avatar } from './Avatar';
-export { Stars } from './Stars';
-export { Badge } from './Badge';
-export { TeacherCard } from './TeacherCard';
