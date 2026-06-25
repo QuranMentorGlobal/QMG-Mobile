2a3229938698a3b47e866b1efe270b8465c3cd26
QuranMentorGlobal <quranmentorglobal@gmail.com>
2026-06-25 06:06:34 +0500

    Delete app/student/messages.tsx

--- app/student/messages.tsx
+++
@@ -1,5 +0,0 @@
-// app/student/messages.tsx
-import { ComingSoon } from '@/components/ComingSoon';
-export default function StudentMessages() {
-  return <ComingSoon title="Messages" icon="chatbubbles-outline" webPath="/platform/messages" />;
-}
