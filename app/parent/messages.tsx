22c043a2dea47b00293565322bddb528a6ad76a3
QuranMentorGlobal <quranmentorglobal@gmail.com>
2026-06-25 06:06:57 +0500

    Delete app/parent/messages.tsx

--- app/parent/messages.tsx
+++
@@ -1,5 +0,0 @@
-// app/parent/messages.tsx
-import { ComingSoon } from '@/components/ComingSoon';
-export default function ParentMessages() {
-  return <ComingSoon title="Messages" icon="chatbubbles-outline" webPath="/platform/messages" />;
-}
